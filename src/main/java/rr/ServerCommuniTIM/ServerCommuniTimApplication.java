5a3403965b12bd55d93f97f397b7d75a521761f5
pozaradugabriel <poza.radu@gmail.com>
2023-01-08 23:39:24 +0200

    initialised spring project

---
+++ src/main/java/rr/ServerCommuniTIM/ServerCommuniTimApplication.java
@@ -0,0 +1,13 @@
+package rr.ServerCommuniTIM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerCommuniTimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerCommuniTimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rr/ServerCommuniTIM/ServerCommuniTimApplicationTests.java
@@ -0,0 +1,13 @@
+package rr.ServerCommuniTIM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerCommuniTimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
