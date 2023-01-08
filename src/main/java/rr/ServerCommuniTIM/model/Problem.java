716ca41dc7d97bb0cd6eaf6d56a5aad91548f7c2
pozaradugabriel <poza.radu@gmail.com>
2023-01-09 00:08:34 +0200

    created problem model

---
+++ src/main/java/rr/ServerCommuniTIM/model/Problem.java
@@ -0,0 +1,63 @@
+package rr.ServerCommuniTIM.model;
+
+public class Problem {
+    private Long id;
+
+    private String category;
+    private String subcategory;
+    private double longitude;
+    private double latitude;
+    public Problem(){
+
+    }
+    public Problem(String category, String subcategory, double longitude, double latitude) {
+        this.category = category;
+        this.subcategory = subcategory;
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(String subcategory) {
+        this.subcategory = subcategory;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Problem{" +
+                "id=" + id +
+                ", category='" + category + '\'' +
+                ", subcategory='" + subcategory + '\'' +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                '}';
+    }
+}
